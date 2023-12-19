9fdb30569a50f8b1d4e6e4477b33dab085feeb86
tanda <21520714@gm.uit.edu.vn>
2023-12-19 16:31:06 +0700

    Playfair

---
+++ Lab6/Bai1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Bai1 : Form
+    {
+        private char[,] keyMatrix;
+
+        public Bai1()
+        {
+            InitializeComponent();
+            initDataGridView();
+        }
+        private void initDataGridView()
+        {
+            guna2DataGridView1.ColumnCount = 5;
+            guna2DataGridView1.RowCount = 5;
+            for (int i = 0; i < 5; i++)
+            {
+                guna2DataGridView1.Columns[i].Width = 20;
+                guna2DataGridView1.Rows[i].Height = 20;
+            }
+        }
+        private String splitText(string input)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                result.Append(input[i]);
+
+                if (i % 2 != 0 && i < input.Length - 1)
+                {
+                    result.Append(" ");
+                }
+            }
+            return result.ToString();
+        }
+        private char[,] GenerateKeyMatrix(string key)
+        {
+            List<char> uniqueChars = key.Distinct().ToList();
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                if (c != 'J' && !uniqueChars.Contains(c))
+                {
+                    uniqueChars.Add(c);
+                }
+            }
+            char[,] matrix = new char[5, 5];
+            int index = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = uniqueChars[index];
+                    index++;
+                }
+            }
+            return matrix;
+        }
+
+        private void DisplayKeyMatrix()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    guna2DataGridView1.Rows[i].Cells[j].Value = keyMatrix[i, j];
+                }
+            }
+        }
+        private string appendSpace(string input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                result.Append(input[i]);
+
+                if (i > 0 && i % 2 != 0)
+                {
+                    result.Append(' ');
+                }
+            }
+
+            return result.ToString();
+        }
+        private void encrypt(string split)
+        {
+            StringBuilder encryptedText = new StringBuilder();
+
+            for (int i = 0; i < split.Length; i += 2)
+            {
+                char char1 = split[i];
+                char char2 = split[i + 1];
+
+                int[] pos1 = GetPosition(char1);
+                int[] pos2 = GetPosition(char2);
+
+                if (pos1[0] == pos2[0])
+                {
+                    encryptedText.Append(keyMatrix[pos1[0], (pos1[1] + 1) % 5]);
+                    encryptedText.Append(keyMatrix[pos2[0], (pos2[1] + 1) % 5]);
+                }
+                else if (pos1[1] == pos2[1])
+                {
+                    encryptedText.Append(keyMatrix[(pos1[0] + 1) % 5, pos1[1]]);
+                    encryptedText.Append(keyMatrix[(pos2[0] + 1) % 5, pos2[1]]);
+                }
+                else
+                {
+                    encryptedText.Append(keyMatrix[pos1[0], pos2[1]]);
+                    encryptedText.Append(keyMatrix[pos2[0], pos1[1]]);
+                }
+            }
+
+            String temp = encryptedText.ToString();
+            temp = appendSpace(temp);
+            guna2TextBox3.Text = temp;
+        }
+
+        private int[] GetPosition(char c)
+        {
+            int[] position = new int[2];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (keyMatrix[i, j] == c)
+                    {
+                        position[0] = i;
+                        position[1] = j;
+                        return position;
+                    }
+                }
+            }
+
+            return position;
+        }
+        private void Bai1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gunaButton2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TabStopChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.ToUpper();
+            textBox1.SelectionStart = textBox1.Text.Length;
+            String input = textBox1.Text.ToUpper();
+            input = input.Replace(" ", "");
+            if (input.Length % 2 != 0)
+            {
+                input = string.Concat(input, "X");
+            }
+            keyMatrix = GenerateKeyMatrix(input);
+            DisplayKeyMatrix();
+        }
+
+        private void Bai1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            guna2TextBox1.Text = guna2TextBox1.Text.ToUpper();
+            guna2TextBox1.SelectionStart = guna2TextBox1.Text.Length;
+            String encryptText = guna2TextBox1.Text.ToUpper();
+            encryptText = encryptText.Replace(" ", "");
+            if (encryptText.Length % 2 != 0)
+            {
+                encryptText = string.Concat(encryptText, "X");
+            }
+            String split = splitText(encryptText);
+            guna2TextBox2.Text = split;
+            encrypt(encryptText);
+        }
+
+        private void guna2DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label5 = new System.Windows.Forms.Label();
+            this.gunaButton2 = new Guna.UI2.WinForms.Guna2Button();
+            this.panelControl = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.panelControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Franklin Gothic Heavy", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(64)))), ((int)(((byte)(137)))));
+            this.label5.Location = new System.Drawing.Point(173, 9);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 26);
+            this.label5.TabIndex = 77;
+            this.label5.Tag = "";
+            this.label5.Text = "Nhóm 5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // gunaButton2
+            // 
+            this.gunaButton2.BackColor = System.Drawing.Color.Transparent;
+            this.gunaButton2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(36)))), ((int)(((byte)(113)))));
+            this.gunaButton2.BorderRadius = 10;
+            this.gunaButton2.BorderThickness = 2;
+            this.gunaButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaButton2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.gunaButton2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.gunaButton2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.gunaButton2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.gunaButton2.FillColor = System.Drawing.Color.Transparent;
+            this.gunaButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(36)))), ((int)(((byte)(113)))));
+            this.gunaButton2.ImageSize = new System.Drawing.Size(35, 35);
+            this.gunaButton2.Location = new System.Drawing.Point(269, 121);
+            this.gunaButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.gunaButton2.Name = "gunaButton2";
+            this.gunaButton2.Size = new System.Drawing.Size(90, 31);
+            this.gunaButton2.TabIndex = 84;
+            this.gunaButton2.Text = "RSA";
+            this.gunaButton2.Click += new System.EventHandler(this.gunaButton2_Click);
+            // 
+            // panelControl
+            // 
+            this.panelControl.Controls.Add(this.label5);
+            this.panelControl.Controls.Add(this.guna2ControlBox2);
+            this.panelControl.Controls.Add(this.guna2ControlBox1);
+            this.panelControl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl.Location = new System.Drawing.Point(0, 0);
+            this.panelControl.Margin = new System.Windows.Forms.Padding(2);
+            this.panelControl.Name = "panelControl";
+            this.panelControl.Padding = new System.Windows.Forms.Padding(11, 2, 0, 0);
+            this.panelControl.Size = new System.Drawing.Size(431, 46);
+            this.panelControl.TabIndex = 85;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.BorderRadius = 5;
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.Black;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(378, 5);
+            this.guna2ControlBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(27, 24);
+            this.guna2ControlBox2.TabIndex = 1;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.BorderRadius = 5;
+            this.guna2ControlBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.Black;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(402, 5);
+            this.guna2ControlBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(27, 24);
+            this.guna2ControlBox1.TabIndex = 0;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this.panelControl;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.AnimateWindow = true;
+            this.guna2BorderlessForm1.AnimationInterval = 600;
+            this.guna2BorderlessForm1.BorderRadius = 30;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(36)))), ((int)(((byte)(113)))));
+            this.guna2Button1.BorderRadius = 10;
+            this.guna2Button1.BorderThickness = 2;
+            this.guna2Button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(36)))), ((int)(((byte)(113)))));
+            this.guna2Button1.ImageSize = new System.Drawing.Size(35, 35);
+            this.guna2Button1.Location = new System.Drawing.Point(73, 121);
+            this.guna2Button1.Margin = new System.Windows.Forms.Padding(2);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(90, 31);
+            this.guna2Button1.TabIndex = 86;
+            this.guna2Button1.Text = "Playfair";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 241);
+            this.Controls.Add(this.gunaButton2);
+            this.Controls.Add(this.panelControl);
+            this.Controls.Add(this.guna2Button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelControl.ResumeLayout(false);
+            this.panelControl.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2Button gunaButton2;
+        private Guna.UI2.WinForms.Guna2Panel panelControl;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+    }
+}
+
